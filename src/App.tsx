beb8654fc5287add69bc41418509dcd2c06934ef
ftmamhmod <ftmamhmod2@gmail.com>
2025-08-30 00:18:39 +0300

    feat: integrate Tailwind CSS and update dependencies
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated package.json to include new dependencies: axios, react-hook-form, react-icons, react-pro-sidebar, react-router-dom, and react-toastify.
    - Modified App.tsx to include Tailwind CSS classes for styling.
    - Created index.css to import Tailwind CSS.
    - Updated vite.config.ts to include Tailwind CSS plugin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <h1 className="bg-amber-500  ">Hello World</h1>
+      <div className="bg-green-500 text-white p-4 text-2xl">
+        Tailwind is now working!
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
