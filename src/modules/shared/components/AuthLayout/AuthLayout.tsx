2f88c9f047b941373ce182eb66a7f832751179b2
Rawan Ahmed <RawanSadek267@gmail.com>
2025-08-30 17:33:44 +0300

    auth layout done

--- src/modules/shared/components/AuthLayout/AuthLayout.tsx
+++ src/modules/shared/components/AuthLayout/AuthLayout.tsx
@@ -1,8 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import logo from '../../../../assets/Images/logo.png'
 const AuthLayout = () => {
+
+  const location = useLocation();
+  const isRegister = location.pathname === "/register";
+
   return (
     <>
-      <Outlet />
+      <div className='authContainer relative'>
+      <div className="h-screen">
+        <div className="flex min-h-[100%] justify-center items-center ">
+          <div className={`${isRegister ? "md:w-[60%]" : "md:w-[40%]"} `}>
+            <div className="logo flex justify-center">
+              <img src={logo} alt="logo" className={`w-70`} />
+            </div>
+            <div className="form-container rounded-md !mt-4.5">
+            <Outlet />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   );
 };
