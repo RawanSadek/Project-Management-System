d0b2be664fdeed47ce31a51530bc98ab6e61e2ee
Rawan Ahmed <RawanSadek267@gmail.com>
2025-08-31 19:03:48 +0300

    feat: implement logout functionality in SideBar component

--- src/modules/shared/components/SidetBar/SideBar.tsx
+++ src/modules/shared/components/SidetBar/SideBar.tsx
@@ -1,5 +1,14 @@
-const SideBar = () => {
-  return <div>SideBar</div>;
-};
+import { useContext } from 'react'
+import { AuthContext } from '../../../../Contexts/AuthContext/AuthContext';
+import { Link } from 'react-router-dom';
 
-export default SideBar;
+export default function SideBar() {
+
+  let { logout } = useContext(AuthContext);
+
+  return (
+    <>
+      <Link to='/login'><button onClick={logout}> Logout </button></Link>
+    </>
+  )
+}
