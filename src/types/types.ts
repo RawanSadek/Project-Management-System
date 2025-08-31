5adb4b563e8fee73c2c8eb84aee6ff6b6c7c56f0
Rawan Ahmed <RawanSadek267@gmail.com>
2025-08-31 17:26:24 +0300

    feat: implement AuthContext and Login component with JWT handling

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,17 @@
+import type { JwtPayload } from "jwt-decode";
+import type { ReactNode } from "react";
+
 export interface loginDataTypes{
     email: string,
     password: string
 }
+
+export interface AuthContextType {
+  loginData: JwtPayload | null;
+  getLoginData: () => void;
+  logout: () => void;
+};
+
+export interface AuthProviderProps {
+  children: ReactNode;
+};
