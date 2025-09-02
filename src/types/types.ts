2c22f2bdf9304c84247bed2ad54fc513a143eab8
ftmamhmod <ftmamhmod2@gmail.com>
2025-09-03 00:49:12 +0300

    feat: implement registration and verification forms with validation and API integration

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,23 +1,36 @@
 import type { JwtPayload } from "jwt-decode";
 import type { ReactNode } from "react";
 
-export interface loginDataTypes{
-    email: string,
-    password: string
+export interface loginDataTypes {
+  email: string;
+  password: string;
 }
 
 export interface AuthContextType {
   loginData: JwtPayload | null;
   getLoginData: () => void;
   logout: () => void;
-};
+}
 
 export interface AuthProviderProps {
   children: ReactNode;
-};
+}
 
-export interface changePassDataTypes{
-    oldPassword: string,
-    newPassword: string,
-    confirmNewPassword: string
+export interface changePassDataTypes {
+  oldPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}
+export interface RegisterTypes {
+  userName: string;
+  email: string;
+  country: string;
+  profileImage?: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+}
+export interface VerifyAccountTypes {
+  email: string;
+  code: string;
 }
--- src/util/axios.ts
+++ src/util/axios.ts
@@ -1,32 +1,29 @@
+import axios from "axios";
 
-import axios from 'axios';
+export const baseURL = `https://upskilling-egypt.com:3003/api/v1`;
 
-
-export const baseURL =`https://upskilling-egypt.com:3003/api/v1`
-
- 
-export const axiosInstance = axios.create({ baseURL,
-   headers:{Authorization:localStorage.getItem("token")}
-})
+export const axiosInstance = axios.create({
+  baseURL,
+  headers: { Authorization: localStorage.getItem("token") },
+});
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization =  token;
+      config.headers.Authorization = token;
     }
     return config;
   },
   (error) => Promise.reject(error)
 );
- 
+
 // USERS_URLS
 
-export const USERS_URLS ={
-    LOGIN: `/Users/Login` ,
-     REGISTER: `/Users/Register` ,
-    FORGET_PASSWORD: `/Users/Reset/Request` ,
-    RESET_PASSWORD: `/Users/Reset` ,
-    VERIFYACCOUNT: `/Users/verify` ,
-    CHANGEPASSWORD: `/Users/ChangePassword`
-   
-}
+export const USERS_URLS = {
+  LOGIN: `/Users/Login`,
+  REGISTER: `/Users/Register`,
+  FORGET_PASSWORD: `/Users/Reset/Request`,
+  RESET_PASSWORD: `/Users/Reset`,
+  VERIFYACCOUNT: `/Users/verify`,
+  CHANGEPASSWORD: `/Users/ChangePassword`,
+};
